1d28a5e260b3613b5acb4a0878776ae82af992af
Nina Volkmuth <ninavolkmuth@gmail.com>
2022-11-20 15:33:43 -0800

    Remove unused import

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,7 @@ import React from "react";
 import {
   BrowserRouter as Router,
   Routes,
-  Route,
-  Link
+  Route
 } from "react-router-dom";
 import { SplashPage } from "./home/SplashPage";
 import './home/SplashPage.css';
